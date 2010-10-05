83ace1a19be23c196e43bcc6705576647efb9330
federicorotolo (Mr ) <federico_rotolo@libero.it>
2010-10-05 15:32:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@67 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/carthwrite.R
@@ -0,0 +1,32 @@
+#############################################################
+#                                                           #
+#   dcarthwrite function                                    #
+#   Author: Federico Rotolo                                 #
+#   Email: federico.rotolo@stat.unipd.it                    #
+#   Date: October, 05, 2010                                 #
+#   Copyright (C) 2010 Federico Rotolo                      #
+#                                                           #
+#   Version                                                 #
+#############################################################
+
+dcarthwrite <- function (x, mu, psi) {
+  if(psi<0){stop("the parameter 'psi' must be non negative")}
+
+  x <- conversion.circular(x, units="radians", zero=0, rotation="counter")
+  mu <- conversion.circular(mu, units="radians", zero=0, rotation="counter")
+  mu <- as.vector(mu)
+  psi <- as.vector(psi)
+
+  attr(x, "class") <- attr(x, "circularp") <-  NULL
+  attr(mu, "class") <- attr(mu, "circularp") <-  NULL    
+  
+  DcarthwrightRad(x, mu, psi)
+}
+
+
+DcarthwrightRad <- function(x, mu, psi) {
+  cpc<-2^(1/psi-1) * (gamma(1+1/psi))^2 * (1+cos(x-mu))^(1/psi)
+  cpc<-cpc/(pi*gamma(1+2/psi))  
+  return(cpc)
+}
+
