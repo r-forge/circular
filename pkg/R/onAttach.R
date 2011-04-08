45ce7d515bce1975f301fda0ee0b17e222c9a79c
claudio (Claudio Agostinelli) <claudio.agostinelli@unitn.it>
2011-04-08 08:05:29 +0000

    Added .onAttach directive
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/onAttach.R
@@ -0,0 +1,14 @@
+.onAttach <- function(library, pkg)
+{
+  # Rv <- R.Version()
+  # if(Rv$major < 2 |(Rv$major == 2 && Rv$minor < 2.0))
+  #  stop("This package requires R 2.2.0 or later")
+  if(interactive())
+  {
+    meta <- packageDescription("circular")
+    packageStartupMessage(
+         "Package 'circular', ", meta$Version, " (", meta$Date, "). ",
+         "Type 'help(Circular)' for summary information")
+  }
+  invisible()
+}
