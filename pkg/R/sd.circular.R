2a96ff64239bca561f648a7cc21c0803db954557
claudio (Claudio Agostinelli) <claudio.agostinelli@unitn.it>
2011-06-10 12:56:45 +0000

    sd.circular returns an object of class circular
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sd.circular.R
+++ pkg/R/sd.circular.R
@@ -7,32 +7,51 @@ sd.data.frame <- function(x, ...) {
     sapply(x, sd, ...)
 }
 
-#############################################################
-#                                                           #
-#   sd.circular function                                    #
-#   Author: Claudio Agostinelli                             #
-#   Email: claudio@unive.it                                 #
-#   Date: May, 17, 2011                                     #
-#   Copyright (C) 2011 Claudio Agostinelli                  #
-#                                                           #
-#   Version 0.1                                             #
-#############################################################
+##############################################################
+#                                                            #
+#   sd.circular function                                     #
+#   Author: Claudio Agostinelli and Jean-Olivier Irisson     #
+#   Email: claudio@unive.it                                  #
+#   Date: June, 10, 2011                                     #
+#   Copyright (C) 2011 Claudio Agostinelli                   #
+#                                                            #
+# As defined in                                              #
+#   Mardia, KV. Statistics of directional data. 1972, p 24,74#
+# Formula actually taken from                                #
+#   Zar, JH. Biostatistical analysis. 2010, sec 26.5, p 617  #
+#                                                            #
+#   Version 0.2                                              #
+##############################################################
 
-sd.circular <- function (x, na.rm=FALSE, ...)  {
+sd.circular <- function (x, na.rm=FALSE, control.circular=list(), ...)  {
   if (is.matrix(x)) {
     apply(x, 2, sd.circular, na.rm=na.rm)
   } else {
     if (na.rm) 
       x <- x[!is.na(x)]
+    if (length(x) == 0) {
+      warning("No observations (at least after removing missing values)")
+      return(circular(NA))
+    }
+    if (!is.circular(x))
+      x <- circular(x)
+    datacircularp <- circularp(x)  
     x <- conversion.circular(x, units="radians", zero=0, rotation="counter")
     attr(x, "class") <- attr(x, "circularp") <-  NULL
-    SdCircularRad(x=x)
+    s <- SdCircularRad(x=x)
+    dc <- control.circular
+    if (is.null(dc$type))
+      dc$type <- datacircularp$type
+    if (is.null(dc$units))
+      dc$units <- datacircularp$units
+    s <- conversion.circular(circular(s), units=dc$units, type=dc$type, template="none", modulo="2pi", zero=0, rotation="counter")
+    return(s)
   }
 }
 
 SdCircularRad <- function(x) {
-  if (any(is.na(x)))
-    return(NA)
+#  if (any(is.na(x)))
+#    return(NA)
 #  n <- length(x)
 #  c <- sum(cos(x))
 #  s <- sum(sin(x))
