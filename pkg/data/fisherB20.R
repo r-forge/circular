598931adf76a0dd263f026fcb95c2abf3ff283d1
claudio (Claudio Agostinelli) <claudio.agostinelli@unitn.it>
2012-09-07 08:13:49 +0000

    Dataset in Fisher 1993 book Appendix B20
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@210 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/data/fisherB20.R
@@ -0,0 +1,11 @@
+
+fisherB20 <- (data.frame(x=cbind(c(67,66,74,61,58,60,100,89,171,166,98,60,197,98,86,123,165,133,101,105,71,84,75,98,83,71,74,91,38,200,56),
+												c(107,46,33,67,122,69,43,30,12,25,37,69,5,83,68,36,21,1,71,60,71,71,57,53,38,70,7,48,7,21,27))))
+names(fisherB20) <- c("theta","x")
+
+#require(circular)
+#fisherB20c <- fisherB20
+#fisherB20c$theta <- circular(fisherB20$theta,units="degrees",type="directions",zero=pi/2,rotation="clock")
+#save(fisherB20,file="fisherB20.rda",ascii=TRUE)
+#save(fisherB20c,file="fisherB20c.rda",ascii=TRUE)
+
