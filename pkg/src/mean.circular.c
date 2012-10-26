035ca3236b74a89b47cd213f4fe0ed62a956112e
ale_gagliardi (Alessandro Gagliardi) <gagliardi@stud.unive.it>
2012-10-26 12:30:40 +0000

    #include <R.h> added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@225 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/mean.circular.c
+++ pkg/src/mean.circular.c
@@ -1,13 +1,11 @@
-/************************************************************
-*																			 	*
+/****************************************************************
+*								*
 *	AUTHORS : CLAUDIO AGOSTINELLI and ALESSANDRO GAGLIARDI	*
-*	AIM : COMPUTE THE MEAN CIRCULAR									*
-*	DATA : 18 OCTOBER 2012.												*
-*																				*
-*************************************************************/
-
-#include <R_ext/Arith.h> //To have NA variable type.
-#include <R_ext/Constants.h>//To have this constant .Machine$double.eps that is labeled "DOUBLE_EPS".
+*	AIM : COMPUTE THE MEAN CIRCULAR				*
+*	DATA : 18 OCTOBER 2012.					*
+*								*
+*****************************************************************/
+#include <R.h>
 
 void MeanCircularRad(double*,int*,double*); //prototype of function
 
