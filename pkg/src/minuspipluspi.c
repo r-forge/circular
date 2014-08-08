bf4188008953e7d21a6694ee9e0e4ef520d91a4f
claudio (Claudio Agostinelli) <claudio.agostinelli@unitn.it>
2014-08-08 15:09:36 +0000

    version 0.4-8.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@283 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/minuspipluspi.c
+++ pkg/src/minuspipluspi.c
@@ -1,9 +1,8 @@
-
 /****************************************************************
 *                                                               *
 *       AUTHORS : CLAUDIO AGOSTINELLI and ALESSANDRO GAGLIARDI  *
-*       AIM : MinusPiPlusPiRad from internal function 			 *
-*					in circular package                       		 *
+*       AIM : MinusPiPlusPiRad from internal function 	        *
+*					in circular package     *
 *       DATA : 02 NOVEMBER 2012.                                *
 *                                                               *
 *****************************************************************/
@@ -12,13 +11,22 @@
 #include <math.h>
 #include "minuspipluspi.h"
 
-void MinusPiPlusPiRad(double *x,int *n)
-{
-	int i;
-	for(i=0;i<(*n);i++)
-	{
-	  	x[i] = (x[i] < -PI)?(x[i] + (2 * PI)):(x[i]);
-		x[i] = (x[i] > PI)?(x[i] - (2 * PI)):(x[i]);
-	}
+void MinusPiPlusPiRadOLD(double *x,int *n) {
+  int i;
+  for (i=0;i<(*n);i++) {
+    if (x[i] < -PI) {
+      x[i] = x[i] + (2 * PI);
+    }
+    if (x[i] > PI) {
+      x[i] = x[i] - (2 * PI);
+    }
+  }
 }
 
+void MinusPiPlusPiRad(double *x,int *n) {
+  int i;
+  for (i=0;i<(*n);i++) {
+     x[i] = (x[i] < -PI)?(x[i] + (2 * PI)):(x[i]);
+     x[i] = (x[i] > PI)?(x[i] - (2 * PI)):(x[i]);
+  }
+}
