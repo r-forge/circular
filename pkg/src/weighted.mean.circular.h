7d296d8cb12622cd493c6554c32b1a830deb65f6
claudio (Claudio Agostinelli) <claudio.agostinelli@unitn.it>
2015-05-12 10:23:48 +0000

    Version 0.4-9. A new function weighted.mean.circular is available. Minor changes in the code of mean.circular, median.circular and medianHL.circular
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/circular@287 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/weighted.mean.circular.h
@@ -0,0 +1,7 @@
+/******************************************************************
+* AUTHORS CLAUDIO AGOSTINELLI
+* AIM : Header of WeightedMeanCircular.c
+* DATA : 12 May 2015
+*******************************************************************/
+
+void WeighetdMeanCircularRad(double*,double*,int*,double*);
